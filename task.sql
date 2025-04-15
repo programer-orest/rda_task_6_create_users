b1ef5654d33729e62a49b274f8572dcfb5f32a3f
Orest Yakymiv <akimivorest@gmail.com>
2025-04-15 19:39:12 +0300

    Create two users for connection to database with different privilleges

--- task.sql
+++ task.sql
@@ -1,1 +1,6 @@
 -- write your code for database user creation here
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';  
+GRANT SELECT, UPDATE, DELETE, INSERT ON ShopDB.* TO 'webappuser'@'%';
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';  
+GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
